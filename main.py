5452e5ba8e835f4a35f1ebca5bc6cb5f35b0e754
eligoodwin <goodwiel@oregonstate.edu>
2017-12-01 20:32:02 -0800

    profilepic is retrieved by service

--- main.py
+++ main.py
@@ -22,10 +22,13 @@ import random
 from google.appengine.ext import ndb
 import time
 import sys
+import tweepy
+import twittercreds
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-#project files
+# project files
 import getTweets
 import parseTweets
 import tweetModel
@@ -33,6 +36,7 @@ import tweetModel
 
 def generateSecret():
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(64))
+
 
 class Secret(ndb.Model):
     """data base for secrets"""
@@ -42,62 +46,83 @@ class Secret(ndb.Model):
 
 class SecretHandler(webapp2.RequestHandler):
     """Responsible for handing out secrets and adding them to the database.
-    Secrets are to be purged every 10 minutes from the database IF they are
-    are secrets in the database"""
+	Secrets are to be purged every 10 minutes from the database IF they are
+	are secrets in the database"""
 
     def get(self):
-        #make a json with the secret and add it to the database of secrets
+        # make a json with the secret and add it to the database of secrets
         newSecret = Secret(secretString=generateSecret(), timeAdded=time.asctime(time.localtime(time.time())))
         newSecret.put()
 
         self.response.write(json.dumps(newSecret.to_dict()))
 
-
+def getUserProfilePic(username):
+    auth = tweepy.OAuthHandler(twittercreds.consumerKey, twittercreds.consumerSecret)
+    auth.set_access_token(twittercreds.accessToken, twittercreds.accessTokenSecret)
+    api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+    userData = api.get_user(username)
+    return userData['profile_image_url_https']
 
 class TweetMaker(webapp2.RequestHandler):
+
     def get(self, username=None):
-    	if username:
-    		theTweets = getTweets.Tweets(username)
-	        theTweets.getLotsOfTweets()
-	        #tweet parser and formatter
-	        tweetsFormatted = parseTweets.TweetsParser()
-	        #generate tweets from corus
-	        makeTweets = tweetModel.DotardTweets()
+        if username:
+            userProfilePic = getUserProfilePic(username)
+            theTweets = getTweets.Tweets(username)
+            theTweets.getLotsOfTweets()
+            # tweet parser and formatter
+            tweetsFormatted = parseTweets.TweetsParser()
+            # generate tweets from corus
+            makeTweets = tweetModel.DotardTweets()
 
-	        theTweets.newTweetData = tweetsFormatted.formatTweets(theTweets.newTweetData)
-	        makeTweets.makeModelFromList(theTweets.newTweetData)
+            theTweets.newTweetData = tweetsFormatted.formatTweets(theTweets.newTweetData)
+            makeTweets.makeModelFromList(theTweets.newTweetData)
 
-	        tweets = makeTweets.generateTweetsFromModel(20)
-	        #test if this is the problem
-	        finalTweets = {'username': username, 'theTweets' : tweets }
-	        self.response.write(json.dumps(finalTweets))
+            tweets = makeTweets.generateTweetsFromModel(20)
+            # test if this is the problem
+            finalTweets = {'username': username, 'userProfilePic': userProfilePic, 'theTweets': tweets}
+            self.response.write(json.dumps(finalTweets))
         else:
-        	self.response.write('error')
+            self.response.write('error')
+
 
 
 
     def post(self):
         requestData = json.loads(self.request.body)
         userData = requestData['username']
-        #print userData
+        # print userData
         theTweets = getTweets.Tweets(userData)
         theTweets.getLotsOfTweets()
-        #tweet parser and formatter
+        # tweet parser and formatter
         tweetsFormatted = parseTweets.TweetsParser()
-        #generate tweets from corus
+        # generate tweets from corus
         makeTweets = tweetModel.DotardTweets()
 
         theTweets.newTweetData = tweetsFormatted.formatTweets(theTweets.newTweetData)
         makeTweets.makeModelFromList(theTweets.newTweetData)
 
         tweets = makeTweets.generateTweetsFromModel(20)
-        #test if this is the problem
-        finalTweets = {'theTweets' : tweets }
+        # test if this is the problem
+        finalTweets = {'theTweets': tweets}
         self.response.write(json.dumps(finalTweets))
+
+
+class Testing(webapp2.RequestHandler):
+    def get(self, username):
+        auth = tweepy.OAuthHandler(twittercreds.consumerKey, twittercreds.consumerSecret)
+        auth.set_access_token(twittercreds.accessToken, twittercreds.accessTokenSecret)
+        api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+        userData = api.get_user(username)
+
+
+        self.response.write(json.dumps(userData))
+
 
 
 allowed_methods = webapp2.WSGIApplication.allowed_methods
 app = webapp2.WSGIApplication([
+    ('/test/(.*)', Testing),
     ('/tweets', TweetMaker),
     ('/tweets/(.*)', TweetMaker),
     ('/secret', SecretHandler)
